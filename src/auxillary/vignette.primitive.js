aff33b5e7bac04d1dd512fd1991fda19a3df3d54
Noeri Huisman <mrxz@users.noreply.github.com>
2022-05-04 21:05:29 +0200

    Reduce whitespace in GLSL string literals

--- src/auxillary/vignette.primitive.js
+++ src/auxillary/vignette.primitive.js
@@ -3,23 +3,22 @@ AFRAME.registerShader('vignette', {
     schema: {
         'intensity': { type: "number", default: 2, max: 10, min: 0, is: 'uniform' }
     },
-    vertexShader: `
-        out vec2 coord;
-        void main() {
-            vec3 newPosition = position * 2.0;
-            coord = vec2(newPosition.x, newPosition.y);
-            gl_Position = vec4(newPosition, 1.0);
-        }
-        `,
-    fragmentShader: `
-        uniform float intensity;
-        in vec2 coord;
-        void main() {
-            float distance = length(coord);
-            distance *= distance;
-            distance *= intensity;
-            gl_FragColor = vec4(0.0, 0.0, 0.0, distance);
-        }`,
+    vertexShader:
+        'out vec2 coord;' +
+        'void main() {' +
+            'vec3 newPosition = position * 2.0;' +
+            'coord = vec2(newPosition.x, newPosition.y);' +
+            'gl_Position = vec4(newPosition, 1.0);' +
+        '}',
+    fragmentShader:
+        'uniform float intensity;' +
+        'in vec2 coord;' +
+        'void main() {' +
+            'float distance = length(coord);'+
+            'distance *= distance;' +
+            'distance *= intensity;' +
+            'gl_FragColor = vec4(0.0, 0.0, 0.0, distance);' +
+        '}',
 });
 
 AFRAME.registerPrimitive('al-vignette', {
